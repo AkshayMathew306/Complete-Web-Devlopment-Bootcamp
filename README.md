0d5dda412f9d688fbaefbbb0c9c82d987e206356
Akshay Mathew <80956455+AkshayMathew306@users.noreply.github.com>
2023-05-07 22:12:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Complete-Web-Devlopment-Bootcamp
+ 
---
+++ tut9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;  
+int main() {
+    // Write C++ code here
+
+int a;
+int b;
+cin>>a>>b;
+if(a>b){
+    cout<<"a is larger";
+}
+    else{
+    cout<<"b is larger";
+    }
+    return 0;
+}
